6fa272111f9f7e0ea3eae004f20c3e10cb744be2
tonny0305 <tonny0305@naver.com>
2024-01-30 14:39:15 +0900

    s

--- Desktop/java1/tutorial/src/tutorial/Tutorial.java
+++ Desktop/java1/tutorial/src/tutorial/Tutorial.java
@@ -5,6 +5,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,World");
+		System.out.println("Hello,Worlds");
 	}
 
 }
